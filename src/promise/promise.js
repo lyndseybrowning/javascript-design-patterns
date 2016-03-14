b109103469ae38b3926cda83a25d3e3db4de0716
Lyndsey Browning <lbrowning86@gmail.com>
2016-03-14 20:39:40 +0000

    Promise api updates

--- src/promise/promise.js
+++ src/promise/promise.js
@@ -1,20 +1,55 @@
 const Promise = function() {
+  let completed = [];
+  let failed = [];
+  let status = 'progress';
+  let data = null;
 
   done(fn) {
+    completed.push(fn);
 
+    if(status === 'done') {
+      fn(data);
+    }
+
+    return this;
   },
 
   failed(fn) {
+    failed.push(fn);
 
+    if(status === 'failed') {
+      fn(data);
+    }
+
+    return this;
   },
 
   resolve(result) {
+    if(status !== 'progress') {
+      throw `Promise has already completed with a status of ${status}`;
+    }
 
+    status = 'done';
+    data = result;
+
+    completed.forEach(function(fn) {
+      fn(data);
+    });
   },
 
   fail(reason) {
+    if(status !== 'progress') {
+      throw `Promise has already completed with a status of ${status}`;
+    }
 
+    status = 'failed';
+    data = reason;
 
+    failed.forEach(function(fn) {
+      fn(data);
+    });
+
+    return this;
   }
 
 };

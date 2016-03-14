f281e42c4399765ee91f84d75a6324ece47b7cde
Lyndsey Browning <lbrowning86@gmail.com>
2016-03-14 20:43:24 +0000

    Added promise object

--- src/promise/promise.js
+++ src/promise/promise.js
@@ -4,53 +4,57 @@ const Promise = function() {
   let status = 'progress';
   let data = null;
 
-  done(fn) {
-    completed.push(fn);
+  const promise = {
+    done(fn) {
+      completed.push(fn);
 
-    if(status === 'done') {
-      fn(data);
+      if(status === 'done') {
+        fn(data);
+      }
+
+      return this;
+    },
+
+    failed(fn) {
+      failed.push(fn);
+
+      if(status === 'failed') {
+        fn(data);
+      }
+
+      return this;
+    },
+
+    resolve(result) {
+      if(status !== 'progress') {
+        throw `Promise has already completed with a status of ${status}`;
+      }
+
+      status = 'done';
+      data = result;
+
+      completed.forEach(function(fn) {
+        fn(data);
+      });
+    },
+
+    fail(reason) {
+      if(status !== 'progress') {
+        throw `Promise has already completed with a status of ${status}`;
+      }
+
+      status = 'failed';
+      data = reason;
+
+      failed.forEach(function(fn) {
+        fn(data);
+      });
+
+      return this;
     }
+  };
 
-    return this;
-  },
-
-  failed(fn) {
-    failed.push(fn);
-
-    if(status === 'failed') {
-      fn(data);
-    }
-
-    return this;
-  },
-
-  resolve(result) {
-    if(status !== 'progress') {
-      throw `Promise has already completed with a status of ${status}`;
-    }
-
-    status = 'done';
-    data = result;
-
-    completed.forEach(function(fn) {
-      fn(data);
-    });
-  },
-
-  fail(reason) {
-    if(status !== 'progress') {
-      throw `Promise has already completed with a status of ${status}`;
-    }
-
-    status = 'failed';
-    data = reason;
-
-    failed.forEach(function(fn) {
-      fn(data);
-    });
-
-    return this;
-  }
+  return promise;
 
 };
 

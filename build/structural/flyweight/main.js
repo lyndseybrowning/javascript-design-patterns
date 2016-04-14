0cfc8c6f026c2fb40679eefed4e96f6c467d5f97
Lyndsey Browning <lbrowning86@gmail.com>
2016-04-14 18:47:34 +0100

    Flyweight pattern demo

--- build/structural/flyweight/main.js
+++ build/structural/flyweight/main.js
@@ -4,44 +4,47 @@
 // it is the act of sharing across objects
 
 var Task = function Task(data) {
-    //this.flyweight = FlyweightFactory.get(data.project, data.priority, data.user, data.completed);
-    this.name = data.name;
-    this.priority = data.priority;
-    this.project = data.project;
-    this.user = data.user;
-    this.completed = data.completed;
+    this.flyweight = FlyweightFactory.get(data.project, data.priority, data.user, data.completed);
+    this.name = data.name; // unique, everything else can become part of a flyweight
+    // this.priority = data.priority;
+    // this.project = data.project;
+    // this.user = data.user;
+    // this.completed = data.completed;
 };
 
-// Task.prototype.getPriority = function () {
-//     this.flyweight.priority;
-// };
-// function Flyweight(project, priority, user, completed) {
-//     this.priority = priority;
-//     this.project = project;
-//     this.user = user;
-//     this.completed = completed;
-// };
+Task.prototype.getPriority = function () {
+    this.flyweight.priority;
+};
 
-// var FlyweightFactory = function () {
-//     var flyweights = {};
-//
-//     var get = function (project, priority, user, completed) {
-//         if (!flyweights[project + priority + user + completed]) {
-//             flyweights[project + priority + user + completed] =
-//                 new Flyweight(project, priority, user, completed);
-//         }
-//         return flyweights[project + priority + user + completed];
-//     };
-//     var getCount = function () {
-//         var count = 0;
-//         for (var f in flyweights) count++;
-//         return count;
-//     }
-//     return{
-//         get: get,
-//         getCount: getCount
-//     }
-// }()
+function Flyweight(project, priority, user, completed) {
+    this.priority = priority;
+    this.project = project;
+    this.user = user;
+    this.completed = completed;
+};
+
+var FlyweightFactory = function () {
+    var flyweights = {};
+
+    var get = function get(project, priority, user, completed) {
+        if (!flyweights[project + priority + user + completed]) {
+            flyweights[project + priority + user + completed] = new Flyweight(project, priority, user, completed);
+        }
+        return flyweights[project + priority + user + completed];
+    };
+
+    var getCount = function getCount() {
+        var count = 0;
+        for (var f in flyweights) {
+            count++;
+        }return count;
+    };
+
+    return {
+        get: get,
+        getCount: getCount
+    };
+}();
 
 function TaskCollection() {
     var tasks = {};
@@ -76,7 +79,7 @@ var completed = [true, false];
 
 var initialMemory = process.memoryUsage().heapUsed;
 
-for (var i = 0; i < 10000; i++) {
+for (var i = 0; i < 100000; i++) {
     tasks.add({
         name: 'task' + i,
         priority: priorities[Math.floor(Math.random() * 5)],
@@ -89,4 +92,4 @@ for (var i = 0; i < 10000; i++) {
 var afterMemory = process.memoryUsage().heapUsed;
 console.log('used memory (MB)', (afterMemory - initialMemory) / 1000000);
 console.log('tasks: ', tasks.getCount());
-//console.log("flyweights: " + FlyweightFactory.getCount());
+console.log("flyweights: " + FlyweightFactory.getCount());
--- src/structural/flyweight/main.js
+++ src/structural/flyweight/main.js
@@ -1,45 +1,48 @@
 // the flyweight pattern should only be used for a large number of objects.
 // it is the act of sharing across objects
 
-var Task = function (data) {
-    //this.flyweight = FlyweightFactory.get(data.project, data.priority, data.user, data.completed);
-    this.name = data.name;
-    this.priority = data.priority;
-    this.project = data.project;
-    this.user = data.user;
-    this.completed = data.completed;
+let Task = function (data) {
+    this.flyweight = FlyweightFactory.get(data.project, data.priority, data.user, data.completed);
+    this.name = data.name; // unique, everything else can become part of a flyweight
+    // this.priority = data.priority;
+    // this.project = data.project;
+    // this.user = data.user;
+    // this.completed = data.completed;
 }
 
-// Task.prototype.getPriority = function () {
-//     this.flyweight.priority;
-// };
-// function Flyweight(project, priority, user, completed) {
-//     this.priority = priority;
-//     this.project = project;
-//     this.user = user;
-//     this.completed = completed;
-// };
+Task.prototype.getPriority = function () {
+    this.flyweight.priority;
+};
 
-// var FlyweightFactory = function () {
-//     var flyweights = {};
-//
-//     var get = function (project, priority, user, completed) {
-//         if (!flyweights[project + priority + user + completed]) {
-//             flyweights[project + priority + user + completed] =
-//                 new Flyweight(project, priority, user, completed);
-//         }
-//         return flyweights[project + priority + user + completed];
-//     };
-//     var getCount = function () {
-//         var count = 0;
-//         for (var f in flyweights) count++;
-//         return count;
-//     }
-//     return{
-//         get: get,
-//         getCount: getCount
-//     }
-// }()
+function Flyweight(project, priority, user, completed) {
+    this.priority = priority;
+    this.project = project;
+    this.user = user;
+    this.completed = completed;
+};
+
+const FlyweightFactory = (() => {
+    let flyweights = {};
+
+    let get = function (project, priority, user, completed) {
+        if (!flyweights[project + priority + user + completed]) {
+            flyweights[project + priority + user + completed] =
+                new Flyweight(project, priority, user, completed);
+        }
+        return flyweights[project + priority + user + completed];
+    };
+
+    let getCount = function () {
+        let count = 0;
+        for (let f in flyweights) count++;
+        return count;
+    }
+
+    return {
+        get: get,
+        getCount: getCount
+    };
+})();
 
 function TaskCollection() {
     let tasks = {};
@@ -65,16 +68,16 @@ function TaskCollection() {
     };
 }
 
-var tasks = new TaskCollection();
+let tasks = new TaskCollection();
 
-var projects = ['none', 'courses', 'training', 'project'];
-var priorities = [1, 2, 3, 4, 5];
-var users = ['Jon', 'Erica', 'Amanda', 'Nathan'];
-var completed = [true, false];
+let projects = ['none', 'courses', 'training', 'project'];
+let priorities = [1, 2, 3, 4, 5];
+let users = ['Jon', 'Erica', 'Amanda', 'Nathan'];
+let completed = [true, false];
 
-var initialMemory = process.memoryUsage().heapUsed;
+let initialMemory = process.memoryUsage().heapUsed;
 
-for (var i = 0; i < 10000; i++) {
+for (let i = 0; i < 100000; i++) {
     tasks.add({
         name: 'task' + i,
         priority: priorities[Math.floor((Math.random() * 5))],
@@ -84,7 +87,7 @@ for (var i = 0; i < 10000; i++) {
     });
 };
 
-var afterMemory = process.memoryUsage().heapUsed;
+let afterMemory = process.memoryUsage().heapUsed;
 console.log('used memory (MB)', (afterMemory - initialMemory) / 1000000);
 console.log('tasks: ', tasks.getCount());
-//console.log("flyweights: " + FlyweightFactory.getCount());
+console.log("flyweights: " + FlyweightFactory.getCount());
